b56f2f7952d84c6f18b1d03f11a26baf1060fe06
Michael-JQP <100227424+Michael-JQP@users.noreply.github.com>
2022-02-22 13:15:57 -0500

    Add files via upload

---
+++ ejLazo-if.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+a = np.array([12, 45, 28, 77, 66, 35])
+
+b = len(a)
+rr = range(b)   #Python 2 se podía utilizar arrange (números flotantes) y range (no flotantes)
+                #Python 3 se unifico se quedo solo con range
+"""
+for i in rr:
+    if a[i] == 66:
+        print("Hola")
+    elif a[i] == 77:
+        print("Lazo de continuidad")
+    elif a[i] <= 28:
+        print("2do Lazo de continuidad")
+    else:
+        print("Chao")
+"""
+
+b = np.array(["Mk", "Mn", "Ms", "Md"])
+l2 = len(b)
+r2 = range(l2)
+
+for i in r2:
+    if b[i] == "Mk":
+        print("Hola otra vez")
+    elif b[i] >= "Mn":
+        print("1er Lazo")
+    else:
+        print("error no cumple condición")
